dc8eba7679f9b88ed5ef63c4a86882cf0c7699fb
Marine Poghosyan <marine.poghosyan@gmail.com>
2024-08-10 17:44:36 +0400

    dev

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService: ConfigService = app.get(ConfigService);
   const port = configService.get('PORT');
+  const config = new DocumentBuilder()
+    .setTitle('title')
+    .setDescription('description')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(port);
 }
 bootstrap();
